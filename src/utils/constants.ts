ab16fc4bbc14d11d59319b65354379a60b51b3a4
QUANG221222 <nguyennhatquang.2509@gmail.com>
2025-12-03 23:19:28 +0700

    Refactor code structure for improved readability and maintainability

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
 import { env } from '~/configs/enviroment'
 
-export const WHITELIST_DOMAINS = ['']
+export const WHITELIST_DOMAINS = ['https://website-selling-cosmetics-v2-fe.vercel.app']
 
 export const WEBSITE_DOMAIN =
   env.BUILD_MODE === 'production'
